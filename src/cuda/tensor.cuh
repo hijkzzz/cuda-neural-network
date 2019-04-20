34783cc513bbf7fcd0689d2830fb1392c90f221a
hijkzzz <janhu9527@gmail.com>
2019-04-20 19:17:16 +0800

    update

--- src/cuda/tensor.cuh
+++ src/cuda/tensor.cuh
@@ -1,17 +1,63 @@
 #pragma once
 
-__global__ void tensor_add(float *a, float *b, float *c, unsigned int width, unsigned int height);
-__global__ void tensor_sub(float *a, float *b, float *c, unsigned int width, unsigned int height);
-__global__ void tensor_mul(float *a, float *b, float *c, unsigned int width, unsigned int height);
-__global__ void tensor_div(float *a, float *b, float *c, unsigned int width, unsigned int height);
+#include <thrust/device_vector.h>
 
-__global__ void tensor_matmul(float *a, float *b, float *c, unsigned int width, unsigned int k, unsigned int height);
-__global__ void tensor_transpose(float *a, float *c, unsigned int width, unsigned int height);
+struct Storage {
+  thrust::device_vector<float> data;
+  thrust::device_vector<std::size_t> shape;
+};
 
-__global__ void tensor_pow(float *a, float *c, unsigned int width, unsigned int height, unsigned int e);
-__global__ void tensor_log(float *a, float *c, unsigned int width, unsigned int height);
-__global__ void tensor_exp(float *a, float *c, unsigned int width, unsigned int height);
-__global__ void tensor_log_softmax(float *a, float *c, unsigned int width, unsigned int height, unsigned int dim);
-__global__ void tensor_mean(float *a, float *c, unsigned int width, unsigned int height, unsigned int dim);
-__global__ void tensor_sigmoid(float *a, float *c, unsigned int width, unsigned int height);
-__global__ void tensor_tanh(float *a, float *c, unsigned int width, unsigned int height);
+void tensor_add(const Storage *a, const Storage *b, Storage *c);
+__global__ void tensor_add_h(const float *a, const float *b, float *c,
+                             std::size_t size);
+
+void tensor_sub(const Storage *a, const Storage *b, Storage *c);
+__global__ void tensor_sub_h(const float *a, const float *b, float *c,
+                             std::size_t size);
+
+void tensor_mul(const Storage *a, const Storage *b, Storage *c);
+__global__ void tensor_mul_h(const float *a, const float *b, float *c,
+                             std::size_t size);
+
+void tensor_div(const Storage *a, const Storage *b, Storage *c);
+__global__ void tensor_div_h(const float *a, const float *b, float *c,
+                             std::size_t size);
+
+void tensor_matmul(const Storage *a, const Storage *b, Storage *c);
+__global__ void tensor_matmul_h(const float *a, const float *b, float *c,
+                                std::size_t *shape_a, std::size_t *shape_b,
+                                std::size_t dims);
+
+void tensor_transpose(const Storage *a, unsigned int dim0, unsigned int dim1,
+                      Storage *c);
+__global__ void tensor_transpose_h(const float *a, unsigned int dim0,
+                                   unsigned int dim1, float *c,
+                                   std::size_t *shape_a, std::size_t dims);
+
+void tensor_log_softmax(const Storage *a, unsigned int dim, Storage *c);
+__global__ void tensor_log_softmax_h(const float *a, unsigned int dim, float *c,
+                                     std::size_t *shape_a, std::size_t dims);
+
+void tensor_mean(const Storage *a, unsigned int dim, Storage *c);
+__global__ void tensor_mean_h(const float *a, unsigned int dim, float *c,
+                              std::size_t *shape_a, std::size_t dims);
+
+void tensor_pow(const Storage *a, unsigned int e, Storage *c);
+__global__ void tensor_pow_h(const float *a, unsigned int e, float *c,
+                             std::size_t *shape_a, std::size_t dims);
+
+void tensor_log(const Storage *a, Storage *c);
+__global__ void tensor_log_h(const float *a, float *c), std::size_t *shape_a,
+    std::size_t dims;
+
+void tensor_exp(const Storage *a, Storage *c);
+__global__ void tensor_exp_h(const float *a, float *c, std::size_t *shape_a,
+                             std::size_t dims);
+
+void tensor_sigmoid(const Storage *a, Storage *c);
+__global__ void tensor_sigmoid_h(const float *a, float *c, std::size_t *shape_a,
+                                 std::size_t dims);
+
+void tensor_tanh(const Storage *a, Storage *c);
+__global__ void tensor_tanh_h(const float *a, float *c, std::size_t *shape_a,
+                              std::size_t dims);
