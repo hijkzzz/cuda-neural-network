bfa31e05ae639db1e09b5efcf2c3d2238d6bff1a
hijkzzz <janhu9527@gmail.com>
2019-04-26 20:23:06 +0800

    update

--- src/cuda/nll_loss.cuh
+++ src/cuda/nll_loss.cuh
@@ -4,5 +4,4 @@
 
 Storage *operator_nll_loss(const Storage *log_p, const Storage *y);
 
-Storage *operator_d_nll_loss(const Storage *log_p, const Storage *y,
-                             const Storage *outputs_grads);
+Storage *operator_d_nll_loss(const Storage *y);
