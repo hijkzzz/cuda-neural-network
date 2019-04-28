9aeca125ea4fddb7e41ca83795f8ed82622713b7
hijkzzz <janhu9527@gmail.com>
2019-04-28 20:21:12 +0800

    add d_log_softmax

--- src/cuda/softmax.cu
+++ src/cuda/softmax.cu
@@ -1,18 +1,16 @@
-#include <softmax.cuh>
+﻿#include <softmax.cuh>
 
-__global__ void operator_log_softmax_h(const float *input1, float *output,
-                                       unsigned int *input1_shape,
-                                       unsigned int input1_dims,
-                                       unsigned int *temp_shape_ptr,
-                                       unsigned int dim, unsigned int dim_stride,
-                                       unsigned int size) {
+__global__ void operator_log_softmax_h(
+    const float *input1, float *output, unsigned int *input1_shape,
+    unsigned int input1_dims, unsigned int *temp_shape, unsigned int dim,
+    unsigned int dim_stride, unsigned int size) {
   unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
 
   if (index < size) {
     unsigned int length = input1_shape[dim];
 
     unsigned int *loc = new unsigned int[input1_dims];
-    index2loc(index, temp_shape_ptr, input1_dims - 1, loc);
+    index2loc(index, temp_shape, input1_dims - 1, loc);
     for (unsigned int i = input1_dims - 1; i > dim; i--) {
       loc[i] = loc[i - 1];
     }
@@ -61,9 +59,74 @@ Storage *operator_log_softmax(const Storage *input1, unsigned int dim) {
       dim dim_stride, size);
 }
 
+__global__ void operator_d_log_softmax_h(
+    const float *input1, const float *output_grads,
+    const unsigned int *input1_shape, const unsigned int *temp_shape,
+    unsigned int input1_dims, unsigned int dim, unsigned int dim_stride,
+    unsigned int size, float *input1_grads) {
+  unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (index < size) {
+    unsigned int length = input1_shape[dim];
+
+    unsigned int *loc = new unsigned int[input1_dims];
+    index2loc(index, temp_shape, input1_dims - 1, loc);
+    for (unsigned int i = input1_dims - 1; i > dim; i--) {
+      loc[i] = loc[i - 1];
+    }
+    loc[dim] = 0;
+    unsigned int base = loc2index(loc, input1_shape, input1_dims);
+    delete[] loc;
+
+    float max_ = -FLT_MIN;
+    for (unsigned int i = 0; i < length; ++i) {
+      max_ = fmaxf(max_, input1[base + i * dim_stride]);
+    }
+
+    double logsum = 0;
+    for (unsigned int i = 0; i < length; ++i) {
+      logsum += expf(input1[base + i * dim_stride] - max_);
+    }
+    logsum = max_ + logf(logsum);
+
+    double dldysum = 0;
+    for (unsigned int i = 0; i < length; ++i) {
+      dldysum += output_grads[base + i * dim_stride];
+    }
+
+    for (unsigned int i = 0; i < length; ++i) {
+      float x = input1[base + i * dim_stride];
+      input1_grads[base + i * dim_stride] =
+          output_grads[base + i * dim_stride] - dldysum * expf(x - logsum);
+    }
+  }
+}
+
 // Y = log_softmax(X) = x - log(exp(X) * 1_n) * 1_n^T
 // dL/dX = dL/dY - (dL/dY * 1_n * exp(x)) / (exp(x) * 1_n)
 Storage *operator_d_log_softmax(const Storage *input1, unsigned int dim,
                                 const Storage *output_grads) {
-  
+  const float *input1_ptr = thrust::raw_pointer_cast(input1->data.data());
+  const float *output_grads_ptr =
+      thrust::raw_pointer_cast(output_grads->data.data());
+  const unsigned int *input1_shape_ptr =
+      thrust::raw_pointer_cast(input1->shape.data());
+  Storage *input1_grads = new Storage(input1->shape);
+  float *input1_grads_ptr = thrust::raw_pointer_cast(input1_grads->data.data());
+
+  thrust::device_vector<unsigned int> temp_shape(input1->shape);
+  temp_shape.erase(temp_shape.begin() + dim);
+  unsigned int *temp_shape_ptr = thrust::raw_pointer_cast(temp_shape.data());
+
+  unsigned int input1_dims = input1->shape.size();
+  unsigned int dim_stride = 1;
+  for (unsigned int i = dim + 1; i < input1_dims; i++) {
+    dim_stride *= input1_shape_ptr[i];
+  }
+
+  unsigned int size = input1->data.size() / input1->shape[dim];
+  unsigned int grid_size = ceil((float)(size) / BLOCK_SIZE);
+  operator_d_log_softmax_h<<<grid_size, BLOCK_SIZE>>>(
+      input1_ptr, output_grads_ptr, input1_shape_ptr, temp_shape_ptr,
+      input1_dims, dim, dim_stride, size, input1_grads_ptr);
 }
--- src/cuda/utils.cuh
+++ src/cuda/utils.cuh
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <cuda_runtime.h>
 #include <iostream>
@@ -6,31 +6,31 @@
 #define BLOCK_SIZE 256
 #define TILE_SIZE 16
 
-#define CHECK_EQ(val1, val2, message)                                          \
-  do {                                                                         \
-    if (val1 != val2)                                                          \
-      throw message;                                                           \
+#define CHECK_EQ(val1, val2, message) \
+  do {                                \
+    if (val1 != val2) throw message;  \
   } while (0)
 
-#define CUDA_KERNEL_LOOP(i, n)                                                 \
-  for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < (n);                 \
+#define CUDA_KERNEL_LOOP(i, n)                                 \
+  for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < (n); \
        i += blockDim.x * gridDim.x)
 
-#define CUDA_CHECK(condition)                                                  \
-  do {                                                                         \
-    cudaError_t error = condition;                                             \
-    CHECK_EQ(error, cudaSuccess, cudaGetErrorString(error));                   \
+#define CUDA_CHECK(condition)                                \
+  do {                                                       \
+    cudaError_t error = condition;                           \
+    CHECK_EQ(error, cudaSuccess, cudaGetErrorString(error)); \
   } while (0)
 
 #define CUDA_POST_KERNEL_CHECK CUDA_CHECK(cudaPeekAtLastError())
 
-template <class T> inline __device__ void swap(T &a, T &b) {
+template <class T>
+inline __device__ void swap(T &a, T &b) {
   T temp = a;
   a = b;
   b = temp;
 }
 
-__device__ void index2loc(unsigned int index, unsigned int *shape,
+__device__ void index2loc(unsigned int index, const unsigned int *shape,
                           unsigned int dims, unsigned int *loc) {
   for (unsigned int i = dims - 1; i >= 0; i--) {
     loc[i] = index % shape[i];
@@ -38,8 +38,9 @@ __device__ void index2loc(unsigned int index, unsigned int *shape,
   }
 }
 
-__device__ unsigned int loc2index(unsigned int *loc, unsigned int *shape,
-                                 unsigned int dims) {
+__device__ unsigned int loc2index(const unsigned int *loc,
+                                  const unsigned int *shape,
+                                  unsigned int dims) {
   unsigned int index = 0;
   unsigned int base = 1;
   for (unsigned int i = dims - 1; i >= 0; i--) {
